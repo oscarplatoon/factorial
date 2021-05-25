b0598e7f921b4797f0d55aff4c79eef4e9b04fb4
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-25 14:20:21 -0500

    Shawn and Stephen Factorial solution

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,20 @@
 exports.factorial = function(num) {
+  let fact = BigInt(1);
+  let bigNum = BigInt(num);
+
+  if (num < 0) {
+    return undefined;
+  }
+  
+  if (fact === 0) {
+    return 1;
+  }
+
+  while (bigNum > 0) {
+    fact = fact * bigNum;
+    bigNum = bigNum - BigInt(1);
+ }
+
+ return Number(fact);
+
 };
