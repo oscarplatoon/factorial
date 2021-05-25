28d5aa8e4549410e034c4b4808840a439fe6a174
Tim Gaston <Gman2105@users.noreply.github.com>
2021-05-25 18:34:31 +0000

    Eric and Tim answer

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,13 @@
-exports.factorial = function(num) {
+exports.factorial = function(num)  {
+   
+   if (num === 0 || num === 1)
+        return 1;
+        
+   for ( let i = num - 1; i >= 1; i--) {
+       num *=i;
+   }
+   
+   return num;
+    
 };
+ 
