d802f3545716ecf90799ab6a228b853fd33930a3
Tianlin Cai <terrylovesbird@users.noreply.github.com>
2021-05-25 20:22:40 +0000

    Tianlin and Jeremiah's solution for factorial

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,14 @@
-exports.factorial = function(num) {
+function factorial(num) {
+    var a = num;
+    //loop from num to 1, and mulitply them all
+    var array=[];
+    for(var i = a; i > 0; i-- ){
+        array.push(i);
+        console.log(array);
+    }
+       
+    return array.reduce( (a, b) => a * b ) ;
+  
 };
+
+console.log(factorial(4));
