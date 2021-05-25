bd1ab87b2bbebb67242a27b3cc16029a24748ac4
jung341416 <jung341416@gmail.com>
2021-05-25 14:50:11 -0400

    Jack Shuff and Jung Park factorial challenge solution

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,12 @@
 exports.factorial = function(num) {
+let i = BigInt(num);
+let multiply = 1n;
+//console.log(multiply);
+while (i > 1) {
+ multiply *= i;
+  i--;
+  // console.log(multiply);
+}
+
+return Number(multiply);
 };
