ceac813cc607e7c1455fdc7a3e5cbbca8f101834
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-26 16:27:16 -0400

    Added 0! case to js

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,15 @@
 exports.factorial = function(num) {
-};
+  if(num == 0) {
+    return 1
+  }
+  let i = BigInt(num);
+  let multiply = 1n;
+  //console.log(multiply);
+  while (i > 1) {
+   multiply *= i;
+    i--;
+    // console.log(multiply);
+  }
+ 
+  return Number(multiply);
+  };
