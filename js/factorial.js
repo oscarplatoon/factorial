6d33523ed8bf04a8f71702179b5fae0bb6abfd98
Ankur Shah <ankur@codeplatoon.org>
2021-05-27 09:58:35 -0500

    solved js solution

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,38 @@
 exports.factorial = function(num) {
+  if (typeof(num) !== 'number') {
+    return null
+  }
+  if (num % 1 !== 0) {
+    return null
+  }
+  if (num < 0) {
+    return null
+  }
+
+  let product = 1
+  for (let i = 2; i <= num; i++) {
+    product *= i
+  }
+
+  return product
+};
+
+exports.bigFactorial = function(num) {
+  if (typeof(num) !== 'bigint') {
+    return null
+  }
+  if (num % 1n !== 0n) {
+    return null
+  }
+  if (num < 0n) {
+    return null
+  }
+  
+  let product = BigInt(1)
+  for (let i = num; i > BigInt(1); i--) {
+    product *= i
+  }
+
+  console.log(product)
+  return product
 };
--- js/factorialSpec.js
+++ js/factorialSpec.js
@@ -1,6 +1,15 @@
 var fact = require("./factorial");
 
-console.log(fact.factorial(8) === 40320);
-console.log(fact.factorial(18) === 6402373705728000);
-console.log(fact.factorial(45) === 119622220865480194561963161495657715064383733760000000000);
+console.log(fact.factorial(-1) == null)
+console.log(fact.factorial("donuts") == null)
+console.log(fact.factorial(4.5) == null)
+console.log(fact.factorial(0) == 1)
+console.log(fact.factorial(1) == 1)
+console.log(fact.factorial(3) == 6)
+console.log(fact.factorial(8) == 40320)
+console.log(fact.factorial(18) == 6402373705728000)
+console.log(fact.factorial(24) == 620448401733239439360000)
+console.log(fact.bigFactorial(BigInt(-3)) == null)
+console.log(fact.bigFactorial(BigInt(45)) == 119622220865480194561963161495657715064383733760000000000n)
+console.log(fact.factorial(50) == 30414093201713378043612608166064768844377641568960512000000000000)
 // Test how high of a number your program can calculate. Can you push it further?
