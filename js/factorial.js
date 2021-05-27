e70d6174e7ac3c66c82606d67a03a4ddfe07f61d
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-05-27 11:03:01 -0400

    Andrew_factorial_python and js solution

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,7 @@
 exports.factorial = function(num) {
+  let answer = 1;
+  for(i=1;i<num+1;i++){
+    answer *= i;
+  }
+  return(answer);
 };
--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,12 @@
 def factorial(num):
-	# your code here
+	if (not isinstance(num, int)):
+		return('Number must be an integer greater than or equal to 0.')
+	elif(num < 0):
+		return('Number must be an integer greater than or equal to 0.')
+	answer = 1
+	while(num>0):
+		answer *= num
+		num -= 1
+	return(answer)
+
+
